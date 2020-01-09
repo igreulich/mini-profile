f21c1beebe341caa51a2a676ff13a8abe133f275
Ian Greulich <ian@greulich.me>
2020-01-09 14:11:12 -0500

    pass in props to ProfileCard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 import ProfileCard from './profile-card/ProfileCard'
 
 const App = () => (
-  <ProfileCard />
+  <ProfileCard
+    name='Ian Greulich'
+    title='Tall human'
+    image='https://retrorpg.files.wordpress.com/2011/04/grue.jpg'
+    content='This is some content.'
+  />
 );
 
 export default App;
