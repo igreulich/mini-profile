ddb2af6b13edb06e15b4df398d5896fd04fbe4ff
Ian Greulich <ian@greulich.me>
2020-01-09 14:10:20 -0500

    pass props from ProfileCard to its child components

--- src/profile-card/ProfileCard.js
+++ src/profile-card/ProfileCard.js
@@ -6,14 +6,18 @@ import ProfileCardProfile from './ProfileCardProfile';
 
 import './ProfileCard.css';
 
-const ProfileCard = () => (
-  <div className="profile-card--container">
-    <ProfileCardAvatar />
-    <div className="profile-card--info-container">
-      <ProfileCardProfile />
-      <ProfileCardContent />
+const ProfileCard = (props) => {
+  const {content, image, name, title} = props;
+
+  return (
+    <div className="profile-card--container">
+      <ProfileCardAvatar image={image} />
+      <div className="profile-card--info-container">
+        <ProfileCardProfile name={name} title={title} />
+        <ProfileCardContent content={content} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfileCard;
